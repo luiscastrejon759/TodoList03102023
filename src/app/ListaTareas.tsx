7b0ba2b0b8ac886183be9bfd614188ebf2b7687b
luiascastrejon759 <joseluis.castrejon4@gmail.com>
2023-10-16 13:46:06 -0600

    Se agrega el contador de tareas completadas

--- src/app/ListaTareas.tsx
+++ src/app/ListaTareas.tsx
@@ -4,16 +4,26 @@ import Tarea from './Tarea';
 export default function Listado(props){ 
 
   let numerotareas = props.Listado.length
+  const [Terminadas, SetTerminadas] = useState(0)
+
+  const aumentarContador = ()  =>{
+    SetTerminadas (prev => prev + 1)
+  }
+
+  const disminuyeContador = ()  =>{
+    SetTerminadas (prev => prev - 1)
+  }
 
   return(  
     <>
     <div>
       
      <ul style={{color:'black'}}>
-         {props.Listado.map(tarea => (<Tarea item={tarea}/>))}
+         {props.Listado.map(tarea => (
+         <Tarea item={tarea} aumentar={aumentarContador} restar={disminuyeContador}/>))}
          
          <h1 style={{color:'black'}}>Total tareas:{numerotareas} </h1> 
-         <h1 style={{color:'black'}}>Tareas Completadas: {} </h1> 
+         <h1 style={{color:'black'}}>Tareas Completadas: {Terminadas} </h1> 
       </ul>  
       </div>
       </>   
--- src/app/Tarea.tsx
+++ src/app/Tarea.tsx
@@ -1,10 +1,19 @@
-import React  from 'react';
+import React, { useState } from 'react';
+
 
 export default function item(props){ 
+  const handleChange = (event) => {                   
+    if (event.target.checked) {
+   
+      props.aumentar()
+    } else {
+      props.restar()         
+    }        
+}; 
   return(  
     <div> 
       
-        <li  key={props.item.id}><input type="checkbox"/>{props.item.name}
+        <li  key={props.item.id}><input type="checkbox" onChange={handleChange}/>{props.item.name}   
         <h1>    
         <button style={{color:'white', fontSize:'20px', border:'none', borderRadius:'4px', background:'red'}}>Editar</button>
         <button style={{color:'white', fontSize:'20px', border:'none', borderRadius:'4px', background:'red'}}>Eliminar</button>
